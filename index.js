060c006924674a1f63ca68a960e4a8c3c8824f6a
vaishali <vaishalisakole7@gmail.com>
2024-08-22 17:06:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// let words=document.querySelectorAll(".word");
+// words.forEach((word)=>)
+// let letters = word.textContent.split("");
+// word.tectContent="";
+// letters-forEach(letter)=>{
+
+// }
+
+
